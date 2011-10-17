bdfebafcf29fdb12d3766789e947a0fff61c9c8f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-17 16:44:00 -0600

    Updated

--- source/prep/infrastructure/filtering/Where.cs
+++ source/prep/infrastructure/filtering/Where.cs
@@ -2,17 +2,10 @@ namespace prep.infrastructure.filtering
 {
     public class Where<ItemToFilter>
     {
-        public static PropertyAccessor<ItemToFilter,PropertyType> has_a<PropertyType>(PropertyAccessor<ItemToFilter,PropertyType> accessor)
+        public static something has_a<PropertyType>(PropertyAccessor<ItemToFilter,PropertyType> accessor)
         {
-            return accessor;
+            throw new NotImplementedException();
         }
     }
 
-    public static class AccessorExtensions
-    {
-        public static IMatchA<ItemToMatch> equal_to<ItemToMatch,PropertyType>(this PropertyAccessor<ItemToMatch,PropertyType> accessor, PropertyType value)
-        {
-            return new AnonymousMatch<ItemToMatch>(x => accessor(x).Equals(value));
-        }
-    }
 }
