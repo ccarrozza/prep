31f58b8dda99bd097f0be5141e3c93fce1cc7dae
git_name <git_email>
2011-10-17 20:54:04 -0400

    Pushing new changes

--- source/prep/infrastructure/filtering/ComparableCriteriaFactory.cs
+++ source/prep/infrastructure/filtering/ComparableCriteriaFactory.cs
@@ -6,14 +6,32 @@ namespace prep.infrastructure.filtering
     {
         Func<ItemToFilter, PropertyType> accessor;
 
+        private readonly CriteriaFactory<ItemToFilter, PropertyType> criteriaFactory; 
+
         public ComparableCriteriaFactory(Func<ItemToFilter, PropertyType> accessor)
         {
             this.accessor = accessor;
+            criteriaFactory = new CriteriaFactory<ItemToFilter, PropertyType>(accessor);
         }
 
         public IMatchA<ItemToFilter> greater_than(PropertyType value)
         {
             return new AnonymousMatch<ItemToFilter>(x => accessor(x).CompareTo(value) > 0);
         }
+
+        public IMatchA<ItemToFilter> equal_to(PropertyType value)
+        {
+            return criteriaFactory.equal_to(value);
+        } 
+
+        public IMatchA<ItemToFilter> equal_to_any(params PropertyType[] values)
+        {
+            return criteriaFactory.equal_to_any(values);
+        } 
+
+        public IMatchA<ItemToFilter> not_equal_to(PropertyType value)
+        {
+            return criteriaFactory.not_equal_to(value);
+        } 
     }
 }
